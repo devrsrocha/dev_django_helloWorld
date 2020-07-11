c152fabaaf98cc04753ab6c9902aeba4ff701c4d
Rodrigo Rocha <devrsrocha@hotmail.com>
2020-07-11 15:44:43 -0300

    Implementado o exercício da aula 1 para somar dois números.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome):
     return HttpResponse('<h1>Hello {}</h1>'.format(nome))
+
+def soma(request, n1, n2):
+    soma = n1 + n2
+    return HttpResponse('<h1>A soma de {} e {} é: {}</h1>'.format(n1, n2, soma))
